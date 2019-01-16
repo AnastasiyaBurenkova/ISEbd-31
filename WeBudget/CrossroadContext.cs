82d58eb0f6960cfba5e33735472692069fa41f24
AnastasiyaBurenkova <nastya.burenkova.98@mail.ru>
2019-01-16 20:42:28 +0400

    6 lab

---
+++ WeBudget/CrossroadContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WeBudget.Models;
+
+namespace WeBudget
+{
+    public class CrossroadContext : DbContext
+    {
+        public DbSet <User> Users { get; set; }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Uslugi> Uslugis { get; set; }
+    }
+}
---
+++ WeBudget/Service/dbService/CarService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WeBudget.Models;
+using WeBudget.Service.Abstract;
+
+namespace WeBudget.Service
+{
+	public class CarService : ICrossroad
+	{
+		CrossroadContext db = new CrossroadContext();
+		public  void Delete(int id) {
+            Car b = db.Cars.Find(id);
+            if (b != null)
+            {
+                db.Cars.Remove(b);
+                db.SaveChanges();
+            }
+        }
+
+        public void Edit(BaseEntity baseEntity) {
+            db.Entry((Car)baseEntity).State = EntityState.Modified;
+            db.SaveChanges();
+        }
+
+        public  void Create(BaseEntity baseEntity) {
+            db.Cars.Add((Car)baseEntity);
+            db.SaveChanges();
+        }
+
+        public BaseEntity findById(int? id)
+        {
+            Car Car = db.Cars.Find(id);
+            return Car;
+        }
+
+        public  List<BaseEntity> getList()
+        {
+            List < BaseEntity > baseentity = new List <BaseEntity>();
+            List<Car> Car = db.Cars.ToList();
+            for (int i = 0; i < Car.Count; i++) {
+                baseentity.Add(Car[i]);
+            }
+            return baseentity;
+        }
+
+    }
+}
